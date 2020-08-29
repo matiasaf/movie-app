1a533e82da691cb08130e88a143cf3e5fe431e28
Matias Fernandez <mfernandez@techinsights.com>
2020-08-29 12:52:48 -0300

    Fixing the privates routes.

--- src/components/private-route/index.js
+++ src/components/private-route/index.js
@@ -2,19 +2,34 @@ import React, { useContext, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { CTX } from '../../Store';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-    const [state, dispatch] = useContext(CTX);
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+//     const [state, dispatch] = useContext(CTX);
+//     return (
+//         <Route
+//             {...rest}
+//             render={(props) =>
+//                 !state.loggedUser ? (
+//                     <Redirect to="/login" />
+//                 ) : (
+//                     <Component {...props} />
+//                 )
+//             }
+//         />
+//     );
+// };
+
+function PrivateRoute({ component: Component, isAuthenticated, ...rest }) {
     return (
         <Route
             {...rest}
             render={(props) =>
-                !state.loggedUser ? (
-                    <Redirect to="/login" />
-                ) : (
+                isAuthenticated ? (
                     <Component {...props} />
+                ) : (
+                    <Redirect to="/login" />
                 )
             }
         />
     );
-};
+}
 export default PrivateRoute;
